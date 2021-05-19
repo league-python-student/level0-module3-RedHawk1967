e7272b1d9ec825211dba45952cd6e581ee6360b9
Jack <37246641+RedHawk1967@users.noreply.github.com>
2021-05-18 18:24:16 -0700

    still on own adventure

--- _01_elif/_4_own_adventure/own_adventure.py
+++ _01_elif/_4_own_adventure/own_adventure.py
@@ -6,7 +6,13 @@ from tkinter import simpledialog, messagebox, Tk
      window.withdraw()
 
 
-
+ start = simpledialog.askstring(title="",prompt="You're walking down a dark path at night and you see a shining light. Do you A, keep walking or B, go to the shining light")
+    if(start == "A"):
+        s2 = simpledialog.askstring(title="",prompt="You walk to the shining light and look up. There's a UFO above you with a tractor beam aimed at you. Do you A, run away or B, let the aliens take you")
+        else if (start == "B")
+            s2b = simpledialog.askstring(title="",prompt="You keep walking ignoring the shining light  then you are suddenly abducted by aliens!."
+                                                         " DO you A keep quiet and don't answer any of their questions or B, teach them about earth and its history")
+            
 # TODO Tell the user a story, but give them options so they can decide the
 #  path of the plot.
 #  Use pop-ups, if statements, and your imagination to make an interesting
